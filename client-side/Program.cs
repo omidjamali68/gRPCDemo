e0955cf17064e408e4ffba7623e86a5312ce45af
omidjamali68 <o.jamali2016@gmail.com>
2023-09-26 14:38:49 +0330

    Prepare client and send first request to server

--- client-side/Program.cs
+++ client-side/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using server_side;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7112/");
+var client = new Greeter.GreeterClient(channel);
+
+var response = await client.SayHelloAsync(
+    new HelloRequest { Name = "Omid" });
+
+Console.WriteLine(response.Message);
+Console.ReadLine();
