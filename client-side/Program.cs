8f5dabed39b11392b504da2d826c23fb29cbb407
omidjamali68 <o.jamali2016@gmail.com>
2023-10-02 13:44:07 +0330

    Add get current weather with stream response

--- client-side/Program.cs
+++ client-side/Program.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 
 var channel = GrpcChannel.ForAddress("https://localhost:7112/");
 
@@ -13,6 +14,20 @@ var weatherResponse = await weatherClient.GetCurrentWeatherAsync(
         City = "London",
         Unit = server_side.Units.Metric
     });
-
 Console.WriteLine(weatherResponse.Temperature);
+
+using (var call = weatherClient.GetCurrentWeatherStream(
+        new server_side.GetCurrentWeatherRequest{
+            City = "London",
+            Unit = server_side.Units.Metric
+        })
+      )
+{
+    while (await call.ResponseStream.MoveNext())
+    {
+        var currentTemperature = call.ResponseStream.Current;
+        Console.WriteLine(currentTemperature.Temperature + " " + currentTemperature.Timestamp);
+    }
+}    
+
 Console.ReadLine();
--- server-side/Services/WeatherService.cs
+++ server-side/Services/WeatherService.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using server_side.Contracts;
 
@@ -6,25 +7,59 @@ namespace server_side.Services;
 
 public class WeatherService : WheatherService.WheatherServiceBase
 {
+    private const string ApiKey = "c0619979f88b002e248e184a2e3f12ca" ;
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly ILogger<WeatherService> _logger;
 
-    public WeatherService(IHttpClientFactory httpClientFactory)
+    public WeatherService(IHttpClientFactory httpClientFactory, ILogger<WeatherService> logger)
     {
         _httpClientFactory = httpClientFactory;
+        _logger = logger;
     }
 
     public override async Task<WeatherResponse> GetCurrentWeather(
         GetCurrentWeatherRequest request, 
         ServerCallContext context)
+    {        
+        Temperatures? temperatures = await GetCurrentTemperatures(request);
+        return new WeatherResponse
+        {
+            Temperature = temperatures!.Main.Temp,
+            FeelsLike = temperatures.Main.FeelsLike,
+            Timestamp = Timestamp.FromDateTime(DateTime.UtcNow)
+        };
+    }
+
+    public override async Task GetCurrentWeatherStream(
+        GetCurrentWeatherRequest request, 
+        IServerStreamWriter<WeatherResponse> responseStream, 
+        ServerCallContext context)
+    {        
+        for (int i = 0; i < 30; i++)
+        {
+            if (context.CancellationToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Request was cancelled by user");
+                break;
+            }
+
+            Temperatures? temperatures = await GetCurrentTemperatures(request);
+            await responseStream.WriteAsync(new WeatherResponse{
+                Temperature = temperatures!.Main.Temp,
+                FeelsLike = temperatures.Main.FeelsLike,
+                Timestamp = Timestamp.FromDateTime(DateTime.UtcNow)
+            });
+            await Task.Delay(1000);
+        }
+    }
+
+    private async Task<Temperatures?> GetCurrentTemperatures(GetCurrentWeatherRequest request)
     {
         var httpClient = _httpClientFactory.CreateClient();
         var responseText = await httpClient.GetStringAsync(
-            $"https://api.openweathermap.org/data/2.5/weather?q={request.City}&appid=c0619979f88b002e248e184a2e3f12ca&units={request.Unit}");
-        
+                    $"https://api.openweathermap.org/data/2.5/weather?q={request.City}&appid={ApiKey}&units={request.Unit}");
+
         var temperatures = JsonSerializer.Deserialize<Temperatures>(responseText);
-        return new WeatherResponse {
-            Temperature = temperatures!.Main.Temp,
-            FeelsLike = temperatures.Main.FeelsLike
-        };
+        return temperatures;
     }
 }
